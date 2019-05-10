d20730974f07ca9c3b2996df3826599ce8b8bbd6
HECTOR ARICEAGA <hector_ariceaga@hotmail.com>
2019-05-10 18:45:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ let findMinAndRemoveSorted = (array) => {
 let merge = (firstArray, secondArray) => {
   let sorted = [];
   while(array1.length != 0 && array2.length != 0) {
-  if (firstArray[0] < secondArray[0]) {
-    sorted.push(findMinAndRemoveSorted(firstArray))
-  } else {
-    sorted.push(findMinAndRemoveSorted(secondArray))
+    if (firstArray[0] < secondArray[0]) {
+      sorted.push(findMinAndRemoveSorted(firstArray))
+    } else {
+      sorted.push(findMinAndRemoveSorted(secondArray))
+    }
   }
   return sorted
 }
