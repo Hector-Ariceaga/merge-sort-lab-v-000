39c32f9107ed2d82b0c3090773d4a9697db7db22
HECTOR ARICEAGA <hector_ariceaga@hotmail.com>
2019-05-10 18:27:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,11 +8,9 @@ let merge = (array1, array2) => {
   let sorted = [];
   let currentMin;
   while(array1.length != 0 && array2.length != 0) {
-    if (findMinAndRemoveSorted(array1) > findMinAndRemoveSorted(array2)) {
-      currentMin = findMinAndRemoveSorted(array1)
-    } else {
-      currentMin = findMinAndRemoveSorted(array2)
-    }
+    let firstMin = findMinAndRemoveSorted(array1);
+    let lastMin = findMinAndRemoveSorted(array2);
+    let currentMin = firstMin > lastMin ? firstMin : lastMin
     sorted.push(currentMin)
   }
   return sorted
